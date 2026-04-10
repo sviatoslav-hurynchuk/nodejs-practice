20af781f87d4fff6f3a5e8e8beef728eb70648ec
Hurynchuk Sviatoslav <cviatoslav83@gmail.com>
2026-04-10 11:41:42 +0300

    add healthcheck

--- lab03/src/app.ts
+++ lab03/src/app.ts
@@ -2,11 +2,21 @@ import express = require('express');
 import cors = require('cors');
 import carRoutes from './routes/car';
 import { errorHandler } from './middleware/errorHandler';
+import mongoose from "mongoose";
 
 const app = express();
 
+app.get('/health', (req, res) => {
+    if (mongoose.connection.readyState === 1) { //connected
+        res.status(200).json({ status: 'OK', database: 'connected' });
+    } else {
+        res.status(503).json({ status: 'ERROR', database: 'disconnected' }); //Service Unavailable
+    }
+});
+
 app.use(cors());
 app.use(express.json());
+
 
 app.use('/cars', carRoutes);
 
